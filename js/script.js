74ffd007305d464862d5049e3a7d56056f0a81ad
Théau Dugueperoux <theaudugueperoux@gmail.com>
2021-11-30 11:54:57 +0100

    start JS

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-let gameTime
-let depopSpeed
-let popSpeed
+let gameTime;
+let depopSpeed = 0;
+let popSpeed = 1000;
+let random;
+let strRan;
+let casing = 64;
+
+for (let i = 0; i < casing; i++) {
+    document.getElementById('grid').innerHTML += '<div class="game__grid" id="'+i+'"></div>';
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
 
 function easy(){
-    
+    popSpeed = 1000
+    depopSpeed = 1000
+    console.log(popSpeed)
 }
 function meduim(){
     
 }
 function hard(){
     
+}
+
+setInterval(() => {
+    random = getRandomInt(casing);
+    document.getElementById(random).innerHTML = '<img src="src/images/JarJar.png" id="JarJar" onclick="punched()" class="game__grid_img" alt="Click">';
+}, popSpeed);
+
+
+function punched(){
+    document.getElementById('JarJar').outerHTML = ''
 }
