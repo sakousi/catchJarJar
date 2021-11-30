ac7b06ba25d95d5d1416a834456b30ce6cdd34df
Théau Dugueperoux <theaudugueperoux@gmail.com>
2021-11-30 12:26:32 +0100

    punch debuged

--- js/script.js
+++ js/script.js
@@ -6,30 +6,35 @@ let strRan;
 let casing = 64;
 
 for (let i = 0; i < casing; i++) {
-    document.getElementById('grid').innerHTML += '<div class="game__grid" id="'+i+'"></div>';
-}
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
+    document.getElementById('grid').innerHTML += '<div class="game__grid" id="' + i + '"></div>';
 }
 
-function easy(){
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function easy() {
     popSpeed = 1000
     depopSpeed = 1000
     console.log(popSpeed)
 }
-function meduim(){
-    
+
+function meduim() {
+
 }
-function hard(){
-    
+
+function hard() {
+
 }
 
 setInterval(() => {
     random = getRandomInt(casing);
-    document.getElementById(random).innerHTML = '<img src="src/images/JarJar.png" id="JarJar" onclick="punched()" class="game__grid_img" alt="Click">';
+
+    document.getElementById(random).innerHTML = '<img src="src/images/JarJar.png" id="jarjar' + random + '" onclick="punched(\'jarjar' + random + '\')" class="game__grid_img" alt="Click">';
 }, popSpeed);
 
 
-function punched(){
-    document.getElementById('JarJar').outerHTML = ''
+function punched(id) {
+    console.log(id)
+    document.getElementById(id).outerHTML = ''
 }
